d8e2d9af0f0f0ef012294845c6dbce7cec468273
shai-daniel <shais.daniel@gmail.com>
2018-01-14 20:25:52 +0200

    lecture 40

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -9,12 +9,19 @@ namespace Vidly.Models
     public class Customer
     {
         public int Id { get; set; }
+
         [Required]
         [StringLength(255)]
         public string Name { get; set; }               
+
         public bool IsSubscribedToNewsletter { get; set; }
+
+        [Display(Name = "Date Of Birth")]
         public DateTime? BirthDate { get; set; }
+
+        [Display(Name = "Membership Type")]
         public MembershipType MembershipType { get; set; }
+
         public byte MembershipTypeId { get; set; }        
     }
 }
