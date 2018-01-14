1a9060e074f8eab14f76f4230b249ea3d9a82d96
shai-daniel <shais.daniel@gmail.com>
2018-01-14 19:55:29 +0200

    lecture 38

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -20,6 +20,11 @@ namespace Vidly.Controllers
             _context.Dispose();
         }
 
+        public ActionResult New()
+        {
+            return View();
+        }
+
 
         public ViewResult Index()
         {
