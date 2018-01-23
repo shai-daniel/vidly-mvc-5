9293a80581e9495e89d3de6c5f5efae641eb978d
shai-daniel <shais.daniel@gmail.com>
2018-01-23 19:35:17 +0200

    validations 1

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -34,6 +34,17 @@ namespace Vidly.Controllers
         [HttpPost]
         public ActionResult Save(Customer customer)
         {
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new CustomerFormViewModel
+                {
+                    Customer = customer,
+                    MembershipTypes = _context.MembershipTypes.ToList()
+                };
+
+                return View("CustomerForm", viewModel);
+            }
+
             if (customer.Id == 0) // new customer
             {
                 _context.Customers.Add(customer);
