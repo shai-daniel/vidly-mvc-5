4bc68edac5c4cdf5b0e62633911eba9c7177e2df
shai-daniel <shais.daniel@gmail.com>
2018-01-30 17:03:54 +0200

    Returning Hierarchical Data

--- Vidly/Controllers/Api/CustomersController.cs
+++ Vidly/Controllers/Api/CustomersController.cs
@@ -7,6 +7,7 @@ using System.Net.Http;
 using System.Web.Http;
 using Vidly.Dto;
 using Vidly.Models;
+using System.Data.Entity;
 
 namespace Vidly.Controllers.API
 {
@@ -22,7 +23,11 @@ namespace Vidly.Controllers.API
         // Get /Api/Customers
         public IHttpActionResult GetCustomers()
         {
-            var customerDtos = _context.Customers.ToList().Select(Mapper.Map<Customer, CustomerDto>);
+            var customerDtos = _context.Customers
+                .Include(c => c.MembershipType)
+                .ToList()
+                .Select(Mapper.Map<Customer, CustomerDto>);
+
             return Ok(customerDtos);
         }
 
--- Vidly/Dto/CustomerDto.cs
+++ Vidly/Dto/CustomerDto.cs
@@ -23,5 +23,8 @@ namespace Vidly.Dto
        
 
         public byte MembershipTypeId { get; set; }
+
+        public MembershipTypeDto MembershipType { get; set; }
+
     }
 }
